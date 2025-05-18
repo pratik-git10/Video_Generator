124077d02900526bb857851a23238927b8d5a9b9
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-05-18 23:57:56 +0530

    added some changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,16 @@
+import Header from "@/components/dashboard/header";
 import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="font-bold flex justify-center items-center ">Hello </h1>
-      <UserButton />
+      <Header />
+      <div className="flex flex-col max-w-3xl mx-auto mt-5 text-center">
+        <h2 className="text-3xl mx-4">Welcome to the world of wizard..</h2>
+        <p className="text-base text-muted-foreground">
+          because magic happens everywhere in every form.
+        </p>
+      </div>
     </div>
   );
 }
